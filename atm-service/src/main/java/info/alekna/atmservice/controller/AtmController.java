14a95fb98fb81433a9c601ad0b23f9c3f1026019
Rytis Alekna <r.alekna@gmail.com>
2022-01-17 14:45:02 +0200

    - Moved atm-service module to its own folder - Added back-code-java-client dependency to pom.xml - Created root pom.xml

--- atm-service/src/main/java/info/alekna/atmservice/controller/AtmController.java
+++ atm-service/src/main/java/info/alekna/atmservice/controller/AtmController.java
@@ -7,8 +7,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
